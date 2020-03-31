a838ddd6fa1cd6612bf992afd34c0379e766b40b
Megan Christine McClure <mcclme02@luther.edu>
2020-03-31 00:29:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,43 +1,9 @@
-# class Dog 
+class Dog 
   
-#   def initialize(name, breed="Mutt")
-#     @name = name 
-#     @breed = breed 
-#   end 
+  def initialize(name, breed="Mutt")
+    @name = name 
+    @breed = breed 
+  end 
   
-# end 
-
-
-
-
-class Dog
-  
-  def initialize(name, breed = "Mutt")
-    @name = name
-    @breed = breed
-  end
-  
-  
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+end 
 
